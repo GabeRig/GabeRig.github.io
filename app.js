0078d7374a62a78051f4b60caf9c32c940a38e9b
Gabe <gabriel.rigaudie@gmail.com>
2024-03-05 19:28:15 +0000

    Introduction of Page numbers
    YIPEEE
    big spotted, needs fixing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const track = document.getElementById("image-track");
+const trackArray = Array.prototype.slice.call( document.getElementById('image-track').children );
 
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
@@ -11,17 +12,17 @@ window.onmouseup = () => {
 }
 
 window.onmousemove = e => {
-    if (track.dataset.mouseDownAt === "0") return;
+    if ( track.dataset.mouseDownAt === "0" ) return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
     const maxMouseDelta = window.innerWidth / 2;
 
-    const percentage = ((mouseDelta / maxMouseDelta) * -100);
+    const percentage = ( ( mouseDelta / maxMouseDelta ) * -100);
     // Ensure nextPercentage stays within the desired range
-	const nextPercentage = Math.max(Math.min(parseFloat(track.dataset.prevPercentage) + percentage, 0), -65);
+	const nextPercentage = Math.max(Math.min( parseFloat( track.dataset.prevPercentage ) + percentage, 0 ), track.dataset.percentageMax );
 
 	track.dataset.percentage = nextPercentage;
-    console.info(track.dataset.percentage);
+    //console.info(track.dataset.percentage);
 
     // Apply the translation to the track
     track.animate({
@@ -29,12 +30,15 @@ window.onmousemove = e => {
     }, { duration: 2000, fill: "forwards" });
 
     // Apply translation to individual images
-    for (const image of track.getElementsByClassName("image")) {
+    for (const image of track.getElementsByClassName( "image" ) ) {
         image.animate({
             transform: `translateX(${3 * nextPercentage}%)`,
             objectPosition: `${100 + nextPercentage}% 50%`
-        }, { duration: 2400, fill: "forwards" }); 
+        }, { duration: 2400, fill: "forwards" });
     }
+
+    updatePageNumber();
+
 }
 
 // // Add scroll wheel functionality
@@ -52,7 +56,7 @@ window.onmousemove = e => {
 //     track.dataset.percentage = nextPercentage;
 //     console.info(track.dataset.percentage);
 
-//     // Apply the translation to the track
+//     // Apply the translation to the image-track
 //     track.animate({
 //         transform: `translate(${nextPercentage}%, -50%)`
 //     }, { duration: 1200, fill: "forwards" });
@@ -61,6 +65,57 @@ window.onmousemove = e => {
 //     for (const image of track.getElementsByClassName("image")) {
 //         image.animate({
 //             objectPosition: `${100 + nextPercentage}% center`
-//         }, { duration: 1200, fill: "forwards" }); 
+//         }, { duration: 1200, fill: "forwards" });
 //     }
 // });
+
+function getClosestImageToCenter( images ) {
+    const viewportCenterX = window.innerWidth / 2;
+    let closestImage = null;
+    let minDistance = Infinity;
+
+    for ( const image of images ) {
+        const imageRect = image.getBoundingClientRect();
+        const imageCenterX = ( imageRect.left + imageRect.right ) / 2;
+        const distanceToCenter = Math.abs( viewportCenterX - imageCenterX );
+
+        if ( distanceToCenter < minDistance ) {
+            minDistance = distanceToCenter;
+            closestImage = image;
+        }
+    }
+
+    return closestImage;
+}
+
+const pageNumber = document.getElementById( "page-number" );
+let closetImageIndex = 0;
+let translatePx = 0;
+
+function updatePageNumber() {
+
+    //function for image closet to center
+    const closestImage = getClosestImageToCenter( track.getElementsByClassName( "image" ) );
+
+    if ( closestImage ) {
+        // Perform actions for the closest image to the center
+        closetImageIndex = trackArray.indexOf( closestImage );
+        translatePx = closetImageIndex * -30;
+        console.log( closetImageIndex );
+    }
+
+
+
+    pageNumber.animate(
+        {
+            transform: `translateY( ${translatePx}px )`
+        },
+        {
+            duration: 2400,
+            easing: "ease-in"
+        }
+    ).onfinish = () => {
+        // This function will be called when the animation finishes
+        pageNumber.style.transform = `translateY( ${translatePx}px )`;
+    };
+}
